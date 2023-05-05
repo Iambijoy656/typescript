b2b64d3f625256f72e8aee82c1ade394410c41f5
iambijoy656 <bijoy.chandrodas56@gmail.com>
2023-05-04 22:43:07 -0700

    ts fuction

--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,10 @@
 //normal function
 //default parameter
 
-function add(num1: number, num2: number=10): number {
+function add(num1: number, num2: number = 10): number {
   return num1 + num2;
 }
-// default parameter always use last item 
+// default parameter always use last item
 add(20);
 
 const addArrow = (num1: number, num2: number): number => num1 + num2;
@@ -12,14 +12,39 @@ const addArrow = (num1: number, num2: number): number => num1 + num2;
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 const newArr = arr.map((ele: number) => ele * ele);
 
-const person:{
-    name:string,
-    balance:number
-    addBalance(money:number):string
-}={
-    name: 'John',
-    balance:100,
-    addBalance(money: number){
-        return `my New Balance: ${this.balance+money}`
-    }
+const person: {
+  name: string;
+  balance: number;
+  addBalance(money: number): string;
+} = {
+  name: "John",
+  balance: 100,
+  addBalance(money: number) {
+    return `my New Balance: ${this.balance + money}`;
+  },
+};
+
+// spared operator
+
+const friends = ["nilam", "joye", "ross"];
+const newFriends = ["monica", "rechel"];
+
+friends.push(...newFriends);
+// console.log(friends);
+
+// rest parameters
+// const greetFriends = (...friends: string[]): void =>
+//   friends.forEach((friend: string) => console.log(`Hi ${friend}`));
+// greetFriends("Bijoy", "Manik", "Monu",'bagla vai');
+
+
+
+const [bestFriends,bondu]=friends
+console.log(bestFriends)
+
+const myBestFriends ={
+    fullName:'abul basar',
+    age:20
 }
+
+const {fullName,age}=myBestFriends
