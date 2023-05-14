709ca4f5f1c882ea0e7a6686a1ac0842cf0e708d
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2023-05-14 22:17:14 +0600

    utilit

--- src/bonus_module/utility.ts
+++ src/bonus_module/utility.ts
@@ -15,3 +15,16 @@ type Contact = Pick<Person, "contactNo" | "email">;
 type Name = Omit<Person, 'email' | 'contactNo'>
 
 //Partial and Required
+//To may all the properties be optional type
+
+type Optional = Partial<Person>
+type RequiredProps = Required<Person>;
+
+
+const person: Person = {
+    name: "joy",
+    email: 'bijoy@gmail.com',
+    contactNo: '5645165'
+}
+
+person.name = 'korim'
