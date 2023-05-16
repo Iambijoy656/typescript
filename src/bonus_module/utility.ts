45c48225506e0dc5668393a2d79cf7f9b6ec4fc8
iambijoy656 <bijoy.chandrodas56@gmail.com>
2023-05-16 05:07:41 -0700

    complete

--- src/bonus_module/utility.ts
+++ src/bonus_module/utility.ts
@@ -21,10 +21,33 @@ type Optional = Partial<Person>
 type RequiredProps = Required<Person>;
 
 
-const person: Person = {
+const person:Readonly <Person> = {
     name: "joy",
     email: 'bijoy@gmail.com',
     contactNo: '5645165'
 }
 
-person.name = 'korim'
+// record type
+
+// type myobj ={
+//     a:string,
+//     b:string,
+//     c:string,
+// }
+
+// using Index signature
+
+// type myobj = {
+//     [key: string]: string;
+// }
+
+
+// Record type
+type myobj = Record<'a'|'b'|'c'|'d',string>
+
+const obj: myobj={
+    a:'1',
+    b:'2',
+    c:'3',
+    d:'0'
+}
--- src/module3/interface.ts
+++ src/module3/interface.ts
@@ -1,66 +1,66 @@
-type User = {
-  name: string;
-  age: number;
-};
+// type User = {
+//   name: string;
+//   age: number;
+// };
 
-type extendedUser = User & {
-  role: string;
-};
+// type extendedUser = User & {
+//   role: string;
+// };
 
-const user2: extendedUser = {
-  name: "joy",
-  age: 908,
-  role: "new user",
-};
+// const user2: extendedUser = {
+//   name: "joy",
+//   age: 908,
+//   role: "new user",
+// };
 
-interface Iuser {
-  name: string;
-  age: number;
-}
-
-interface IextendedUser extends Iuser {
-  role: string;
-}
-
-const users: IextendedUser = {
-  name: "Bijoy",
-  age: 90,
-  role: "unknown",
-};
-
-// const userWithAlias:User={
-//     name:"Type alias",
-//     age:100,
+// interface Iuser {
+//   name: string;
+//   age: number;
 // }
 
-// const userWithInterface:Iuser={
-//     name:"Type interface",
-//     age:200,
+// interface IextendedUser extends Iuser {
+//   role: string;
 // }
 
-//----------------------------------------------------------------//
+// const users: IextendedUser = {
+//   name: "Bijoy",
+//   age: 90,
+//   role: "unknown",
+// };
 
-//Object , function, Array
+// // const userWithAlias:User={
+// //     name:"Type alias",
+// //     age:100,
+// // }
 
-//useing alias type
-type addNumberType = (num1: number, num2: number) => number;
-const addNumbers: addNumberType = (num1, num2) => num1 + num2;
+// // const userWithInterface:Iuser={
+// //     name:"Type interface",
+// //     age:200,
+// // }
 
-//useing interface type
-interface IAddNumbers {
-  (num1: number, num2: number): number;
-}
-const addNumber: IAddNumbers = (num1, num2) => num1 + num2;
+// //----------------------------------------------------------------//
 
-// number type array
-const rollNumbers: number[] = [1, 2, 5];
+// //Object , function, Array
 
-// using type alias array
-type rollNumberType = number[];
-const rollNumber: rollNumberType = [1, 3, 5];
+// //useing alias type
+// type addNumberType = (num1: number, num2: number) => number;
+// const addNumbers: addNumberType = (num1, num2) => num1 + num2;
 
-// using interface type array
-interface IRollNumbers {
-  [index: number]: string;
-}
-const rollNumberss: IRollNumbers = ["3", "4", "0"];
+// //useing interface type
+// interface IAddNumbers {
+//   (num1: number, num2: number): number;
+// }
+// const addNumber: IAddNumbers = (num1, num2) => num1 + num2;
+
+// // number type array
+// const rollNumbers: number[] = [1, 2, 5];
+
+// // using type alias array
+// type rollNumberType = number[];
+// const rollNumber: rollNumberType = [1, 3, 5];
+
+// // using interface type array
+// interface IRollNumbers {
+//   [index: number]: string;
+// }
+// const rollNumberss: IRollNumbers = ["3", "4", "0"];
